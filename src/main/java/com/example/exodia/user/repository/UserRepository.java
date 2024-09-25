7eb23d4f37c1259c1da4420bf78af3c4ce56d99b
ki-wing <dldpsk151@gmail.com>
2024-09-25 15:58:22 +0900

    build: ADD TEST

--- src/main/java/com/example/exodia/user/repository/UserRepository.java
+++ src/main/java/com/example/exodia/user/repository/UserRepository.java
@@ -4,9 +4,11 @@ import com.example.exodia.common.domain.DelYN;
 import com.example.exodia.position.domain.Position;
 import com.example.exodia.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserNum(String userNum);
     Optional<User> findByUserNumAndDelYn(String userNum, DelYN delYn);
--- src/main/java/com/example/exodia/userDelete/repository/DeleteHistoryRepository.java
+++ src/main/java/com/example/exodia/userDelete/repository/DeleteHistoryRepository.java
@@ -2,6 +2,8 @@ package com.example.exodia.userDelete.repository;
 
 import com.example.exodia.userDelete.domain.DeleteHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DeleteHistoryRepository extends JpaRepository<DeleteHistory, Long> {
 }
